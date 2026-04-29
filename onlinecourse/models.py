38b7e60ce121e99c6d6cba68d1a9c66992255ce0
feramig <feram_caa@hotmail.com>
2026-04-28 22:11:46 -0400

    add: adding question  models

--- onlinecourse/models.py
+++ onlinecourse/models.py
@@ -101,3 +101,26 @@ class Enrollment(models.Model):
 #class Submission(models.Model):
 #    enrollment = models.ForeignKey(Enrollment, on_delete=models.CASCADE)
 #    choices = models.ManyToManyField(Choice)
+
+#Question Model
+class Question (models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    question_text = models.CharField(max_length=300)
+    grade = models.IntegerField(default=50)
+
+    def __str__(self):
+        return "Question: " + self.question_text
+    
+    # method to calculate if the learner gets the score of the question
+    def is_get_score(self, selected_ids):
+        all_answers = self.choice_set.filter(is_correct=True).count()
+        selected_correct = self.choice_set.filter(is_correct=True, id__in=selected_ids).count()
+        if all_answers == selected_correct:
+            return True
+        else:
+            return False
+
+            
+#Choice Model
+class Choice (models.Model):
+
