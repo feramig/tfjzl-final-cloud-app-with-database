8f5075d88c29376415aa188d304aaa3704cf3c91
feramig <feram_caa@hotmail.com>
2026-04-28 23:06:50 -0400

    add: adding Choice and Submission Models

--- onlinecourse/models.py
+++ onlinecourse/models.py
@@ -94,14 +94,6 @@ class Enrollment(models.Model):
     mode = models.CharField(max_length=5, choices=COURSE_MODES, default=AUDIT)
     rating = models.FloatField(default=5.0)
 
-
-# One enrollment could have multiple submission
-# One submission could have multiple choices
-# One choice could belong to multiple submissions
-#class Submission(models.Model):
-#    enrollment = models.ForeignKey(Enrollment, on_delete=models.CASCADE)
-#    choices = models.ManyToManyField(Choice)
-
 #Question Model
 class Question (models.Model):
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
@@ -120,7 +112,25 @@ class Question (models.Model):
         else:
             return False
 
-            
+
 #Choice Model
 class Choice (models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    content = models.CharField(max_length=300)
+    is_correct = models.BooleanField(default = False)
+
+
+
+# One enrollment could have multiple submission
+# One submission could have multiple choices
+# One choice could belong to multiple submissions
+class Submission(models.Model):
+    enrollment = models.ForeignKey(Enrollment, on_delete=models.CASCADE)
+    choices = models.ManyToManyField(Choice)
+# One enrollment could have multiple submission
+# One submission could have multiple choices
+# One choice could belong to multiple submissions
+class Submission(models.Model):
+    enrollment = models.ForeignKey(Enrollment, on_delete=models.CASCADE)
+    choices = models.ManyToManyField(Choice)
 
